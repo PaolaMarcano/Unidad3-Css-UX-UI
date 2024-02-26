02c6178050eca98e42b1b9ebcfa712ec860f68ee
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-02-26 09:44:25 -0400

    Crear páginas faltantes

--- routes/dulces_detalles.js
+++ routes/dulces_detalles.js
@@ -7,6 +7,14 @@ router.get('/productos',function(req, res, next){
   res.sendFile(path.resolve(__dirname,"../views/productos.html"))
 })  
 
+router.get('/productos_regalos',function(req, res, next){
+  res.sendFile(path.resolve(__dirname,"../views/productos-regalos.html"))
+})  
+
+router.get('/productos_decoraciones',function(req, res, next){
+  res.sendFile(path.resolve(__dirname,"../views/productos-decoraciones.html"))
+})  
+
 
 router.get('/carrito',function(req, res, next){
   res.sendFile(path.resolve(__dirname,"../views/carrito.html"))
