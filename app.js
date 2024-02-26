b7f031c5550ac1aa9697f55233ebf65cb8145301
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-02-26 00:13:52 -0400

    Crear página de productos

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var registroRouter = require('./routes/registro_curso');
+var paginaRouter = require('./routes/dulces_detalles')
 
 var app = express();
 
@@ -19,7 +20,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
+
 app.use('/registrarse', registroRouter);
+app.use('/dulces_detalles', paginaRouter);
 
 
 // catch 404 and forward to error handler
