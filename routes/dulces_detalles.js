0b425296db025f5e1b51b203a2b861a26dc2a4ee
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-02-26 06:56:50 -0400

    Crear carrito de compras

--- routes/dulces_detalles.js
+++ routes/dulces_detalles.js
@@ -7,4 +7,9 @@ router.get('/productos',function(req, res, next){
   res.sendFile(path.resolve(__dirname,"../views/productos.html"))
 })  
 
+
+router.get('/carrito',function(req, res, next){
+  res.sendFile(path.resolve(__dirname,"../views/carrito.html"))
+})  
+
 module.exports = router; 
